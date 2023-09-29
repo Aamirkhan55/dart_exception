4f0cbf4ed610bb92babb35680c335086e5ba7385
Amir55143 <aamirnoor55143@gmail.com>
2023-09-29 23:04:38 +0500

    first commit

---
+++ bin/dart_exception.dart
@@ -0,0 +1,5 @@
+import 'package:dart_exception/dart_exception.dart' as dart_exception;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_exception.calculate()}!');
+}
---
+++ test/dart_exception_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_exception/dart_exception.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
